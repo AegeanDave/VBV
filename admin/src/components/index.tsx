78829885538e8879065b6aa59db723fa857da8d5
Haohai Deng <hh389677@dal.ca>
2024-01-15 16:48:53 -0400

    feat: Remove phone input

--- admin/src/components/index.tsx
+++ admin/src/components/index.tsx
@@ -3,13 +3,5 @@ import Navigator from "./Navigator/index";
 import ScreenDialog from "./Dialog/index";
 import Popup from "./Popup/index";
 import CustomizedSwitch from "./CustomSwitch";
-import PhoneInput from "./PhoneInput";
 
-export {
-  ProductTable,
-  Navigator,
-  ScreenDialog,
-  Popup,
-  CustomizedSwitch,
-  PhoneInput,
-};
+export { ProductTable, Navigator, ScreenDialog, Popup, CustomizedSwitch };
